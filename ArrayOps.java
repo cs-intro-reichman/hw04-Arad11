f8e4fd2027e332db6a6a58f38a1b44d76ff2ce94
Arad <arad.koluok@post.runi.ac.il>
2024-01-11 08:50:46 +0200

    all functions

--- ArrayOps.java
+++ ArrayOps.java
@@ -4,23 +4,84 @@ public class ArrayOps {
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int expectedSum = calcInvoiceSeriesSum(array);
+        int actualSum = 0;
+        for(int i = 0; i < array.length; i++) {
+            actualSum += array[i];
+        }
+        return (expectedSum - actualSum);
+    }
+
+    private static int calcInvoiceSeriesSum(int [] array) {
+        int first = array[0];
+        int n = array.length;
+        int d = 1;
+        double sum = (n / 2) * (2*first + (n-1)*d);
+        return (int)sum;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int maxNum = 0;
+        int secondMaxNum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxNum) {
+                secondMaxNum = maxNum;
+                maxNum = array[i];
+            }
+            else if (array[i] > secondMaxNum) {
+                secondMaxNum = array[i];
+            }
+        }   
+        return secondMaxNum;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        boolean isExsists = false;
+        for(int i = 0; i < array1.length; i++) {
+            isExsists = false;
+            for( int j = 0; j < array2.length; j++) {
+               if (array1[i] == array2[j]) {
+                 isExsists = true;
+               }
+
+               if (!isExsists) {
+                return false;
+               }
+            }
+        }
+
+        for(int i = 0; i < array2.length; i++) {
+            isExsists = false;
+            for( int j = 0; j < array1.length; j++) {
+               if (array2[i] == array1[j]) {
+                 isExsists = true;
+               }
+
+               if (!isExsists) {
+                return false;
+               }
+            }
+        }
+
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        if (array[0] > array[1]) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
+                    return false;
+                }
+            }
+        }
+        else if(array[0] < array[1]) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i+1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
