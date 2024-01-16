8b973d8dbd9989d56889cd56149dddd745f1567e
Arad <arad.koluok@post.runi.ac.il>
2024-01-16 08:12:10 +0200

    func

--- StringOps.java
+++ StringOps.java
@@ -22,17 +22,90 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        // System.out.println("z " + Integer.valueOf('z'));
+        // System.out.println("a " + Integer.valueOf('a'));
+        // System.out.println("e " + Integer.valueOf('e'));
+        // System.out.println("i " + Integer.valueOf('i'));
+        // System.out.println("o " + Integer.valueOf('o'));
+        // System.out.println("u " + Integer.valueOf('u'));
+        // System.out.println("A " + Integer.valueOf('A'));
+        // System.out.println("Z " + Integer.valueOf('Z'));
+        System.out.println(camelCase("Hello World"));
+        System.out.println(camelCase("HELLO world"));
+        System.out.println(camelCase(" tWo wordS"));
+        System.out.println(camelCase("world"));
+        System.out.println(camelCase("“ Intro to coMPUter sCIEncE "));
     }
-
+    
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String newStr = "";
+        int currentCharValue;
+        for (int i = 0; i < string.length(); i++) {
+            currentCharValue = Integer.valueOf(string.charAt(i));
+            if ( currentCharValue == 32) {
+                continue;
+            }
+            if(currentCharValue == 97 || currentCharValue == 101 || currentCharValue == 111 || currentCharValue == 117 || currentCharValue == 105) {
+                newStr += (char)(currentCharValue - 32);
+            }
+            else if(currentCharValue == 65 || currentCharValue == 62 || currentCharValue == 79 || currentCharValue == 85 || currentCharValue == 73) {
+                newStr += (char)(currentCharValue);
+            }
+            else if (currentCharValue >= 65 && currentCharValue <= 90){
+                newStr += (char)(currentCharValue + 32);
+            }
+            else if (currentCharValue >= 97 && currentCharValue <= 122) {
+                newStr += (char)(currentCharValue);
+            }
+        }
+
+        return newStr;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String newStr = "";
+        char currentChar;
+        int charValue;
+        int prevValue = -1;
+        boolean isFirstLetter = false;
+        for (int i = 0; i < string.length(); i++) {
+            currentChar = string.charAt(i);
+            charValue = Integer.valueOf(currentChar);
+            if (i > 0) {
+                prevValue = Integer.valueOf(string.charAt(i - 1));
+            }
+            if ( charValue == 32 ) {
+                continue;
+            }
+            else if (!isFirstLetter) {
+                if (charValue >= 97 && charValue <= 122) {
+                    newStr = newStr + currentChar;
+                }
+                else if (charValue >= 65 && charValue <= 90) {
+                    newStr = newStr + (char)(charValue + 32);
+                }
+
+                isFirstLetter = true;
+            }
+            else if (prevValue == 32) {
+                if (charValue >= 97 && charValue <= 122) {
+                    newStr = newStr + (char)(charValue - 32);
+                }
+                else if (charValue >= 65 && charValue <= 90) {
+                    newStr = newStr + currentChar;
+                }
+            }
+            else if (prevValue != 32) {
+                if (charValue >= 97 && charValue <= 122) {
+                    newStr = newStr + currentChar;
+                }
+                else if (charValue >= 65 && charValue <= 90) {
+                    newStr = newStr + (char)(charValue + 32);
+                }
+            }
+        }
+        
+        return newStr;
     }
 
     public static int[] allIndexOf (String string, char chr) {
